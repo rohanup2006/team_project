9c006b3961f3d7c7100eb969960bd50b8fd722b6
rohanup <rohanup2006@gmail.com>
2026-06-09 09:04:48 +0530

    Added multiplication,division,floor division

--- calculator.py
+++ calculator.py
@@ -1,5 +1,12 @@
-import pandas as pd 
-df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
-def add(a, b):
-    return a + b
-print(add(df['a'], df['b']))
+def multiply(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error! Division by zero is not allowed."
+    return a / b
+
+def floor_division(a, b):
+    if b == 0:
+        return "Error! Division by zero is not allowed."
+    return a // b
